d4b11a7ee401dfffacf61aace1726799dcf3fe86
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2023-11-14 06:21:02 +0800

    remove extra html codes

--- utility/auxilliary.py
+++ utility/auxilliary.py
@@ -4,7 +4,4 @@ def whitespaces(n):
     for _ in range(n):
         st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
-Create_Dataset = """<p>To have a more convenient approach in using the APP, you can create the table
-using the table provided with the appropriate format. Please strictly follow the data format to be able
-to use the Dashboard App. It should be noted that expression units on the guidelines section.</p>"""
 
